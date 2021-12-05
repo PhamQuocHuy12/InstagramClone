dfd89e2ee8f2826f1facdaf384f26c315f453369
PhamQuocHuy12 <1801040104@s.hanu.edu.vn>
2021-12-05 09:19:58 +0700

    fixed TakePicture

--- app/screens/TakePicture.js
+++ app/screens/TakePicture.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import {Camera} from 'expo-camera';
 import * as ImagePicker from 'expo-image-picker';
 import {Icon} from 'react-native-elements';
+import { useIsFocused } from '@react-navigation/native';
 import {
   StyleSheet,
   Text,
@@ -10,6 +11,7 @@ import {
   View,
   Image,
   Button,
+  ToastAndroid,
 } from 'react-native';
 
 export default function TakePicture({navigation}) {
@@ -18,74 +20,73 @@ export default function TakePicture({navigation}) {
   const [camera, setCamera] = useState(null);
   const [image, setImage] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
+  const isFocused = useIsFocused();
 
   useEffect(() => {
     (async () => {
       const cameraStatus = await Camera.requestCameraPermissionsAsync();
       setHasCameraPermission(cameraStatus.status === 'granted');
 
-      const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      const galleryStatus =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
       setHasGalleryPermission(galleryStatus.status === 'granted');
     })();
   }, []);
 
   const takePicture = async () => {
     if (camera) {
-      const data = await camera.takePictureAsync();
+      const data = await camera.takePictureAsync(null);
       setImage(data.uri);
-      console.log(image);
-      toAddCaption(data.uri);
+      navigation.navigate('AddCaption', {image: data.uri});
     }
-  }
+  };
 
   const pickImage = async () => {
+    try{
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [1, 1],
       quality: 1,
     });
+  
     console.log(result);
 
     if (!result.cancelled) {
       setImage(result.uri);
-      toAddCaption(result.uri);
+      navigation.navigate('AddCaption', {image: result.uri});
     }
-  };
-
-  const toAddCaption = (image) => {
-    navigation.navigate('AddCaption', {image: image});
+  }catch(error){
+    ToastAndroid.show("Cannot select PNG file", ToastAndroid.SHORT);
   }
+  };
 
   if (hasCameraPermission === null || hasGalleryPermission === false) {
     return <View />;
   }
-  if (hasCameraPermission === null || hasGalleryPermission === false) {
+  if (hasCameraPermission === false || hasGalleryPermission === false) {
     return <Text>No access to camera</Text>;
   }
   return (
     <View style={styles.container}>
       <View style={styles.cameraContainer}>
-        <Camera
+      { isFocused &&<Camera
           ref={ref => setCamera(ref)}
           style={styles.camera}
           type={type}
           ratio={'1:1'}
-        />
+        />}
       </View>
       {/* {image && <Image source={{uri: image}} style={styles.image} />} */}
-
       <View style={styles.cameraControlContainer}>
         <TouchableOpacity onPress={() => pickImage()}>
           <Icon reverse name="photograph" type="fontisto" />
         </TouchableOpacity>
-
         <TouchableOpacity
           style={styles.takePictureBtn}
           onPress={() => takePicture()}>
           <Icon reverse name="camera" type="evilicons" />
         </TouchableOpacity>
-
         <TouchableOpacity
           onPress={() => {
             setType(
